7dddfc4515bc05dd608de66ee0d14d89b9b3a13c
oleksiishamarin <derynia@gmail.com>
2025-03-18 23:35:37 +0100

    Added widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Oleksii Shamarin on 18/03/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
