7e074b746ff3fe3eafc17ce9cbe35416230a3633
oleksiishamarin <derynia@gmail.com>
2025-03-18 16:00:17 +0100

    Added pokemon detail

--- Dex3/ContentView.swift
+++ Dex3/ContentView.swift
@@ -134,7 +134,8 @@ struct ContentView: View {
                     pokedex.nsPredicate = dynamicPredicate
                 }
                 .navigationDestination(for: Pokemon.self) { pokemon in
-                    Text(pokemon.name ?? "No name")
+                    PokemonDetail()
+                        .environmentObject(pokemon)
                 }
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
