7e7204a6c1f7968a3eba20bd6f952d955bc8be8b
oleksiishamarin <derynia@gmail.com>
2025-03-19 00:28:48 +0100

    Finished with CoreData and Widget.

--- Dex3/Persistence.swift
+++ Dex3/Persistence.swift
@@ -50,6 +50,11 @@ struct PersistenceController {
         container = NSPersistentContainer(name: "Dex3")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            container.persistentStoreDescriptions.first!.url = FileManager
+                .default
+                .containerURL(forSecurityApplicationGroupIdentifier: "group.com.oleksii.shamarin.DexGroup")!
+                .appendingPathComponent("Dex3.sqlite")
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
