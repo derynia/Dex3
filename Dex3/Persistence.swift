9b7cacfff2aa9dd6edf2708619c7c77ff7335529
oleksiishamarin <derynia@gmail.com>
2025-03-20 14:59:41 +0100

    Create model container for persistence

--- Dex3/Persistence.swift
+++ Dex3/Persistence.swift
@@ -5,74 +5,28 @@
 //  Created by Oleksii Shamarin on 21/02/2025.
 //
 
-import CoreData
+import SwiftData
+import Foundation
 
+@MainActor
 struct PersistenceController {
-    static let shared = PersistenceController()
     static var previewPokemon: Pokemon {
-        let context = PersistenceController.preview.container.viewContext
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
         
-        let fetchRequest: NSFetchRequest<Pokemon> = Pokemon.fetchRequest()
-        fetchRequest.fetchLimit = 1
+        let pokemonData = try! Data(contentsOf: Bundle.main.url(forResource: "saamplepokemon", withExtension: "json")!)
         
-        let results = try! context.fetch(fetchRequest)
-        return results.first!
+        let pokemon = try! decoder.decode(Pokemon.self, from: pokemonData)
+        
+        return pokemon
     }
 
-    static let preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newPokemon = Pokemon(context: viewContext)
-            newPokemon.id = 1
-            newPokemon.name = "bulbasaur"
-            newPokemon.types = ["grass", "poison"]
-            newPokemon.hp = 45
-            newPokemon.attack = 49
-            newPokemon.defence = 49
-            newPokemon.specialAttack = 65
-            newPokemon.speed = 45
-            newPokemon.spriteURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png")
-            newPokemon.shinyURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png")
-                        
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            print(error)
-        }
-        return result
+    // Sample preview DB
+    static let preview: ModelContainer = {
+        let container = try! ModelContainer(for: Pokemon.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        container.mainContext.insert(previewPokemon)
+        
+        return container
     }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "Dex3")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        } else {
-            container.persistentStoreDescriptions.first!.url = FileManager
-                .default
-                .containerURL(forSecurityApplicationGroupIdentifier: "group.com.oleksii.shamarin.DexGroup")!
-                .appendingPathComponent("Dex3.sqlite")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
 }
